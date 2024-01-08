ddbc1078db34680188158a4fc9dc1e123377b469
elberrariwiam2001 <wiam.elberrari@etu.uae.ac.ma>
2024-01-08 13:01:04 +0100

    firs

---
+++ projetfin/src/main/java/com/example/projetfin/ProjetfinApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetfin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetfinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetfinApplication.class, args);
+	}
+
+}
